1a9fc522d1de6c0a6599b028cf05d4526baeebbe
Uros Pocek <uros.pocek@gmail.com>
2022-06-07 01:57:53 +0200

    feat [back]: Added database connection

--- APP/API/Login/Repository/LoginRepository.cs
+++ APP/API/Login/Repository/LoginRepository.cs
@@ -1,7 +1,32 @@
+using MySqlConnector;
+
 public class LoginRepository : ILoginRepository
 {
+    MySqlConnector.MySqlConnectionStringBuilder builder;
+
+    public LoginRepository()
+    {
+        builder = new MySqlConnectionStringBuilder
+        {
+            Server = "your-server",
+            UserID = "database-user",
+            Password = "P@ssw0rd!",
+            Database = "database-name",
+        };
+    }
     public async Task InsertUser(User user)
     {
+        using var connection = new MySqlConnection(builder.ConnectionString);
+        await connection.OpenAsync();
 
+        using var command = connection.CreateCommand();
+        command.CommandText = @"SELECT * FROM myuser;";
+
+        using var reader = await command.ExecuteReaderAsync();
+        while (reader.Read())
+        {
+            var email = reader.GetString("email");
+            Console.WriteLine(email);
+        }
     }
 }
