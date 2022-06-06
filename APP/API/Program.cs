b1898e74052912c6daceb75afd57b75baf3be6e8
Uros Pocek <uros.pocek@gmail.com>
2022-06-06 22:30:38 +0200

    feat [back]: Added required packages

--- APP/API/Program.cs
+++ APP/API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddCors(options =>
                                                   .AllowAnyMethod(); ;
                       });
 });
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
