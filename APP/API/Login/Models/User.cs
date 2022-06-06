3f562b37ef8ebcc5ca009faafdbd4f39f93eb737
Uros Pocek <uros.pocek@gmail.com>
2022-06-06 22:00:10 +0200

    BREAKING CHANGE [back] : Backend initial setup

---
+++ APP/API/Login/Models/User.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+public class User
+{
+    public string? _Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+}
