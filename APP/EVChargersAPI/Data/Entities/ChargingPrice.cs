96b62a0c4d510af0c5d26052b3e2006dd9f7bb13
Bojan Mijanovic <mijanovic.boki.bojan@gmail.com>
2022-06-12 18:07:21 +0200

    <feature> [back]: Added models

---
+++ APP/EVChargersAPI/Data/Entities/ChargingPrice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("ChargingPrice")]
+    public class ChargingPrice
+    {
+        [Column("id")]
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [Column("dateFrom")]
+        [JsonPropertyName("dateFrom")]
+        public DateTime DateFrom { get; set; }
+
+        [Column("price")]
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+
+        [Column("station")]
+        [JsonPropertyName("station")]
+        public Guid StationId { get; set; }
+
+    }
+}
---
+++ APP/EVChargersAPI/Data/Entities/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    [Table("Reservation")]
+    public class Reservation
+    {
+        [Column("id")]
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [Column("reservationDate")]
+        [JsonPropertyName("reservationDate")]
+        public DateTime ReservationDate { get; set; }
+
+        [Column("person")]
+        [JsonPropertyName("person")]
+        public Guid PersonId { get; set; }
+
+        [Column("charger")]
+        [JsonPropertyName("charger")]
+        public Guid ChargerId { get; set; }
+    }
+}
